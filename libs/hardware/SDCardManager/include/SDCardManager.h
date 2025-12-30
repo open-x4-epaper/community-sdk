1083bdabd68254a9ebbbf6447ad92fb65334474c
gebeto <slavik.nychkalo@gmail.com>
2025-12-30 17:50:49 +0200

    add SdManager rename method

--- libs/hardware/SDCardManager/include/SDCardManager.h
+++ libs/hardware/SDCardManager/include/SDCardManager.h
@@ -29,6 +29,7 @@ class SDCardManager {
   bool exists(const char* path) { return sd.exists(path); }
   bool remove(const char* path) { return sd.remove(path); }
   bool rmdir(const char* path) { return sd.rmdir(path); }
+  bool rename(const char* path, const char* newPath) { return sd.rename(path, newPath); }
 
   bool openFileForRead(const char* moduleName, const char* path, FsFile& file);
   bool openFileForRead(const char* moduleName, const std::string& path, FsFile& file);
